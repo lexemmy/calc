41f9347afb93afac4341ecc21fd2010efe30eec7
lexemmy <emmy007.el@gmail.com>
2020-05-28 03:02:22 +0100

    update

--- process/indexDB.php
+++ process/indexDB.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "yds";
+    $servername = "us-cdbr-east-05.cleardb.net";
+    $username = "bc994d5aac4363";
+    $password = "941cc0e5";
+    $dbname = "heroku_0b3288a9dbdd471";
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
@@ -11,3 +11,4 @@
         die("Connection failed: " . $conn->connect_error);
     }
 ?>
+
