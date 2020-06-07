5af4a4fa48b517d9e83319294e1e7682fc337f70
lexemmy <emmy007.el@gmail.com>
2020-06-07 12:56:19 +0100

    m

--- dashboard.php
+++ dashboard.php
@@ -45,7 +45,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="user.php">
+        <a class="nav-link" href="Dashboard.php">
           <i class="fa fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
